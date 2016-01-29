1c5ce52469a43e7fa099748851be89ad610f2238
승원 <milk5590@naver.com>
2016-01-29 15:37:15 +0900

    일부 수정

--- src/main/java/milk/floatingtext/FloatingText.java
+++ src/main/java/milk/floatingtext/FloatingText.java
@@ -1,15 +1,53 @@
 package milk.floatingtext;
 
+import cn.nukkit.Player;
+import cn.nukkit.block.Block;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.entity.Entity;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.Position;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
+import milk.floatingtext.text.Text;
+
+import java.util.HashMap;
 
 public class FloatingText extends PluginBase implements Listener{
 
+    HashMap<String, String> data = new HashMap<>();
+
     @Override
     public void onEnable(){
+        Entity.registerEntity(Text.class, true);
         this.getServer().getLogger().info(TextFormat.GOLD + "[FloatingText]플러그인이 로드 되었습니다");
+    }
+
+    @EventHandler
+    public void onPlayerBlockEvent(PlayerInteractEvent ev){
+        if(ev.getFace() == 255 || ev.getAction() != PlayerInteractEvent.RIGHT_CLICK_BLOCK){
+            return;
+        }
+
+        Player player = ev.getPlayer();
+        /*if(!data.containsKey(player.getName().toLowerCase())){
+            return;
+        }*/
+
+        Position pos = ev.getBlock().getSide(ev.getFace());
+        Text.create(/*data.get(player.getName().toLowerCase())*/"스트리이잉", pos);
+        System.out.println("시바아아아아앍");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        String output = "[FloatingText]";
+        sender.sendMessage(output);
+        return true;
     }
 
 }
--- src/main/java/milk/floatingtext/text/Text.java
+++ src/main/java/milk/floatingtext/text/Text.java
@@ -1,4 +1,108 @@
 package milk.floatingtext.text;
 
-public class Text{
+import cn.nukkit.Player;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.data.ByteEntityData;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
+import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.DoubleTag;
+import cn.nukkit.nbt.tag.FloatTag;
+import cn.nukkit.nbt.tag.ListTag;
+import cn.nukkit.network.protocol.AddPlayerPacket;
+
+public class Text extends Entity{
+
+    long timeout;
+
+    public Text(FullChunk chunk, CompoundTag nbt, String text, Long second){
+        super(chunk, nbt);
+
+        this.timeout = second;
+        this.setNameTag(text);
+        this.setNameTagVisible();
+        this.setDataProperty(DATA_NO_AI, new ByteEntityData((byte) 1));
+    }
+
+    @Override
+    public int getNetworkId(){
+        return NETWORK_ID;
+    }
+
+    public static Text create(String text, Position pos){
+        return create(text, pos, Long.MAX_VALUE);
+    }
+
+    public static Text create(String text, Position pos, long second){
+        FullChunk chunk = pos.getLevel().getChunk(((int) pos.x) >> 4, ((int) pos.z) >> 4, true);
+        if(chunk == null){
+            return null;
+        }
+
+        if(!chunk.isGenerated()){
+            chunk.setGenerated();
+        }
+
+        if(!chunk.isPopulated()){
+            chunk.setPopulated();
+        }
+
+        CompoundTag nbt = new CompoundTag()
+            .putList(new ListTag<DoubleTag>("Pos")
+                .add(new DoubleTag("", pos.x))
+                .add(new DoubleTag("", pos.y))
+                .add(new DoubleTag("", pos.z)))
+            .putList(new ListTag<DoubleTag>("Motion")
+                .add(new DoubleTag("", 0))
+                .add(new DoubleTag("", 0))
+                .add(new DoubleTag("", 0)))
+            .putList(new ListTag<FloatTag>("Rotation")
+                .add(new FloatTag("", 0))
+                .add(new FloatTag("", 0)));
+
+        Entity k = Entity.createEntity("Text", chunk, nbt, text, second);
+        if(k == null){
+            return null;
+        }
+
+        k.spawnToAll();
+        return (Text) k;
+    }
+
+    @Override
+    public void spawnTo(Player player){
+        if(
+            !this.hasSpawned.containsKey(player.getLoaderId())
+            && player.usedChunks.containsKey(Level.chunkHash(this.chunk.getX(), this.chunk.getZ()))
+        ){
+            AddPlayerPacket pk = new AddPlayerPacket();
+            pk.eid = this.getId();
+            pk.x = (float) this.x;
+            pk.y = (float) this.y - 0.75f;
+            pk.z = (float) this.z;
+            pk.speedX = pk.speedY = pk.speedZ = 0;
+            pk.yaw = (float) this.yaw;
+            pk.pitch = (float) this.pitch;
+            pk.metadata = this.dataProperties;
+            player.dataPacket(pk);
+
+            this.hasSpawned.put(player.getLoaderId(), player);
+        }
+    }
+
+    @Override
+    protected void updateMovement(){
+
+    }
+
+    @Override
+    public boolean onUpdate(int tick){
+        if(--this.timeout < 0){
+            this.close();
+            return false;
+        }
+        return true;
+    }
+
 }
